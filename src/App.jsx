36f68349fd017c2c7517268cfbb7d70d722a686f
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-07-08 23:03:33 +0530

    feat: add App.css stylesheet link in index.html and clean up App component imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 // import Login from "./pages/login.jsx";
 import Home from "./pages/Home";
-import YouTubeHome from "./pages/YoutubeHome.jsx";
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
-    // <>
-    //   <div>App</div>
-    // </>
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
         {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/yt" element={<YouTubeHome />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
