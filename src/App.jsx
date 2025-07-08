a47054bc99513768fe2352d94d4fbdf157e10ad6
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-07-08 18:39:09 +0530

    feat: integrate Tailwind CSS and remove unused components
    - Added Tailwind CSS and related dependencies to package.json.
    - Removed custom CSS styles in App.css and replaced with Tailwind import.
    - Commented out Login route and added YouTubeHome route in App.jsx.
    - Deleted unused ApiSettings and Backend components.
    - Updated vite.config.js to include Tailwind CSS plugin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
-import Login from "./pages/login.jsx";
+// import Login from "./pages/login.jsx";
 import Home from "./pages/Home";
+import YouTubeHome from "./pages/YoutubeHome.jsx";
 
 function App() {
   return (
+    // <>
+    //   <div>App</div>
+    // </>
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/yt" element={<YouTubeHome />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
