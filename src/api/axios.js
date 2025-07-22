f9ff7907e3aac012553b24d66e8c78d9ce6110ae
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-07-22 17:28:18 +0530

    fix backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import { backend_local, backend_prod } from "./_config.js";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
   // baseURL: `${backend_local}/api/v1`, 
-  // baseURL: `${backend_prod}/api/v1`, 
+  baseURL: `${backend_prod}/api/v1`, 
   withCredentials: true, // sends cookies
 });
 
